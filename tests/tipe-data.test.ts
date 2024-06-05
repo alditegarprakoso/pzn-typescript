5bce67d155d4c236c750520163f75d132646114c
Aldi Tegar Prakoso <alditegarprakoso@gmail.com>
2024-06-05 09:40:37 +0700

    tipe data primitif (string, boolean, number)

---
+++ tests/tipe-data.test.ts
@@ -0,0 +1,12 @@
+describe('Data Type', () => { 
+    it('should mush declare', () => {
+        const name: string = 'Aldi Tegar Prakoso'
+        const balance: number = 1000000
+        const isVip: boolean = true
+
+        console.info(name);
+        console.info(balance);
+        console.info(isVip);
+        
+    })
+ })
