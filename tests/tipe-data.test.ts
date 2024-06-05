da30bb06b88952f7807f3a01d7065a0b8e8972cb
Aldi Tegar Prakoso <alditegarprakoso@gmail.com>
2024-06-05 10:29:08 +0700

    fix include exclude for development

--- tests/tipe-data.test.ts
+++ tests/tipe-data.test.ts
@@ -1,12 +1,12 @@
-describe('Data Type', () => { 
-    it('should mush declare', () => {
-        const name: string = 'Aldi Tegar Prakoso'
-        const balance: number = 1000000
-        const isVip: boolean = true
+describe("Data Type", () => {
+  it("should mush declare", () => {
+    let name: string = "Aldi Tegar Prakoso";
+    let balance: number = 1000000;
+    let isVip: boolean = true;
 
-        console.info(name);
-        console.info(balance);
-        console.info(isVip);
-        
-    })
- })
+    console.info(name);
+    console.info(balance);
+    console.info(isVip);
+    
+  });
+});
