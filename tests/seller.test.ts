d69e6c332fd7c8d10aca1f4351243bc08d0f2a9b
Aldi Tegar Prakoso <alditegarprakoso@gmail.com>
2024-06-06 09:24:41 +0700

    readonly properties

--- tests/seller.test.ts
+++ tests/seller.test.ts
@@ -6,12 +6,18 @@ describe("Interface", () => {
       id: 1,
       name: "Aldi",
       address: "Bogor",
+      nib: "1234567890",
+      npwp: "1234567890",
     };
 
     console.info(seller);
 
     seller.id = 2;
     seller.name = "Tegar";
+    seller.address = "Jakarta";
+    // NIB dan NPWP tidak bisa diubah karena readonly
+    // seller.nib = "0987654321";
+    // seller.npwp = "0987654321";
     console.info(seller);
   });
 });
