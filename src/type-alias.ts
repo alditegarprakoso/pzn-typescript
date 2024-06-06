fe82a62f986d533b10a49d36d815e118cad24b92
Aldi Tegar Prakoso <alditegarprakoso@gmail.com>
2024-06-06 09:01:14 +0700

    optional type (?)

--- src/type-alias.ts
+++ src/type-alias.ts
@@ -1,13 +1,15 @@
-export type ID = string | number
+export type ID = string | number;
 
 export type Category = {
-    id: ID;
-    name: string;
-}
+  id: ID;
+  name: string;
+  description?: string; // Tanda tanya ini untuk optional type (boleh diisi atau tidak)
+};
 
 export type Product = {
-    id: ID;
-    name: string;
-    price: number;
-    category: Category
-}
+  id: ID;
+  name: string;
+  price: number;
+  category: Category;
+  description?: string; // Tanda tanya ini untuk optional type (boleh diisi atau tidak)
+};
--- tests/object.test.ts
+++ tests/object.test.ts
@@ -1,7 +1,8 @@
 describe("Object", () => {
   it("should name with javascript", () => {
     // Create Object
-    const person: { id: number; name: string } = {
+    // Tanda tanya pada hobbies itu optional type (boleh diisi atau tidak)
+    const person: { id: number; name: string, hobbies?: string[] } = {
       id: 1,
       name: "Aldi",
     };
